26bd42097d8d4a18cb39180a49ca344f0b9129ea
Robin Peerlinck <robinpeerlinck@gmail.com>
2022-11-15 12:17:18 +0100

    Level 1 Hint

--- src/fn-piglatintranslator/lambda.js
+++ src/fn-piglatintranslator/lambda.js
@@ -44,6 +44,18 @@ async function sendToTeams(message) {
         teamName: process.env.TeamName // Team name is given as an environment variable
     };
 
+    let eventBridgeParams = {
+        Entries: [
+            {
+                Detail: JSON.stringify(messageToSend),
+                DetailType: "SendToTeams",
+                Resources: [ process.env.TeamName ],
+                Source: "HTF22",
+                EventBusName: process.env.EventBusName
+            }
+        ]
+    }
+
 }
 
 async function sendToSendGrid(message) {
